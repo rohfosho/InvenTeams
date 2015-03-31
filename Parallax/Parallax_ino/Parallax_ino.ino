cbd08a082867346ad1a0109c40c29608576b8f94
Rohith Varanasi <rohvar98@gmail.com>
2015-03-31 11:53:02 -0400

    lol

--- Parallax/Parallax_ino/Parallax_ino.ino
+++ Parallax/Parallax_ino/Parallax_ino.ino
@@ -1,3 +1,4 @@
+#include <elapsedMillis.h>
 #include <SoftwareSerial.h>
 #include <SF02.h>
 
@@ -10,28 +11,30 @@
 SF02 sf02;
 SoftwareSerial sf02Serial(SERIAL_RX_PIN, SERIAL_TX_PIN);
 
-/*
-  In this example. We setup communications with the SF02 using 
-  the SoftwareSerial interface. If no interface is specified, the
-  default hardware serial interface will be used.
-  
-  An on-demand request for a distance reading can then eaily 
-  be made from the SF02 module, by simply calling getDistance()
-  on the SF02 instance.
-*/
+elapsedMillis timer();
+#define interval 50
+float prevReading = 0.0;
+float velocity = 0.0;
 
 void setup()
 {
   Serial.begin(TERMINAL_MONITOR_BAUDRATE);
-  
+  timer0 = 0;
   sf02Serial.begin(sf02.getAuxUartBaudRate());
   sf02.begin(sf02Serial);
+  sf02.set00vDistance(0.0);
   sf02.setAnalogInputPin(1);
 }
 
 void loop()
 {
-  Serial.print("Distance: ");
-  Serial.print(sf02.getAnalogDistance());
-  Serial.println(" m");
+  if(timer0 > interval){
+    timer0-=interval;
+    prevReading = sf02.getAnalogDistance()-0.80;
+    Serial.print("Distance: ");
+    Serial.print(sf02.getAnalogDistance()-0.80);
+    Serial.println(" m");
+
+  }
 }
+
