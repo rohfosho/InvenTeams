3257fd0583fc2d700bef102df3d33b7823d3e8d7
Nishil Shah <nishil.shah17@gmail.com>
2015-04-01 12:01:03 -0400

    added libraries

--- Parallax/Parallax_ino/Parallax.ino
+++ Parallax/Parallax_ino/Parallax.ino
@@ -7,12 +7,18 @@
 // Arduino Pin Definitions - Set these to whatever suits your Ardunio configuration.
 #define SERIAL_RX_PIN      11
 #define SERIAL_TX_PIN      10
+//#define SERIAL_RX_PIN_2 #
+//#define SERIAL_TX_PIN_2 #0
 
-SF02 sf02;
-SoftwareSerial sf02Serial(SERIAL_RX_PIN, SERIAL_TX_PIN);
+SF02 RF1;
+SoftwareSerial rf1Serial(SERIAL_RX_PIN, SERIAL_TX_PIN);
+SF02 RF2;
+SoftwareSerial rf2Serial(SERIAL_RX_PIN_2, SERIAL_TX_PIN_2);
 
 elapsedMillis timer();
-#define interval 50
+#define interval 20
+int timer0;
+float currentReading;
 float prevReading = 0.0;
 float velocity = 0.0;
 
@@ -20,26 +26,28 @@ void setup()
 {
   Serial.begin(TERMINAL_MONITOR_BAUDRATE);
   timer0 = 0;
-  sf02Serial.begin(sf02.getAuxUartBaudRate());
-  sf02.begin(sf02Serial);
-  sf02.set00vDistance(0.0);
-  sf02.setAnalogInputPin(1);
+  rf1Serial.begin(RF1.getAuxUartBaudRate());
+  RF1.begin(RF1);
+  RF1.set00vDistance(0.0);
+  RF1.setAnalogInputPin(1);
 }
 
 void loop()
 {
   if(timer0 > interval){
     timer0-=interval;
-    prevReading = sf02.getAnalogDistance()-0.80;
+    prevReading = RF1.getAnalogDistance()-0.80;
     delay(10);
     Serial.print("Distance: ");
-    currentReading = sf02.getAnalogDistance()-0.80;
+    currentReading = RF1.getAnalogDistance()-0.80;
     Serial.print(currentReading);
     if(prevReading != currentReading) {
       //not detecting something that is stationary
       //can beep 
     }
     Serial.println(" m");
+    
+    
 
   }
 }
