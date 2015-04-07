fad8d1f9ecdd36decc43e1df55ef9757066979e0
Nishil Shah <nishil.shah17@gmail.com>
2015-04-07 19:55:06 -0400

    solving bugs left and right

--- Parallax/Parallax.ino
+++ Parallax/Parallax.ino
@@ -8,6 +8,8 @@
 #define SERIAL_TX_PIN      10
 #define SERIAL_RX_PIN_2     6
 #define SERIAL_TX_PIN_2     5
+#define ANALOG_PIN_1        1
+#define ANALOG_PIN_2        2
 
 SF02 RF1;
 SoftwareSerial rf1Serial(SERIAL_RX_PIN, SERIAL_TX_PIN);
@@ -21,7 +23,7 @@ float curReading1, curReading2;
 float prevReading1 = 0.0, prevReading2 = 0.0;
 float d1, d2, dx;
 float v = 0.0;
-float x = 0.1778; //distance in meters between rangefinders
+float x = 0.127; //distance in meters between rangefinders
 float minChange = 1.0;
 float minVelocity = 2.0;
 boolean detected = false;
@@ -32,40 +34,38 @@ void setup()
   rf1Serial.begin(RF1.getAuxUartBaudRate());
   RF1.begin(rf1Serial);
   RF1.set00vDistance(0.0);
-  RF1.setAnalogInputPin(1);
+  RF1.setAnalogInputPin(ANALOG_PIN_1);
   rf2Serial.begin(RF2.getAuxUartBaudRate());
   RF2.begin(rf2Serial);
   RF2.set00vDistance(0.0);
-  RF2.setAnalogInputPin(2);
+  RF2.setAnalogInputPin(ANALOG_PIN_2);
 }
 
 void loop()
 {
     delay(100);
-    curReading1 = RF1.getAnalogDistance()-0.8;
-    curReading2 = RF2.getAnalogDistance()-0.8;
+    curReading1 = RF1.getAnalogDistance()-2.8;
+    curReading2 = RF2.getAnalogDistance()-2.8;
     if(prevReading1 < 0.05 && prevReading2 < 0.05) {
       prevReading1 = curReading1;
       prevReading2 = curReading2;
     }
-    /* Serial.print("RF1 Distance: ");
-    Serial.print(curReading1);
-    Serial.println(" m");
-    Serial.print("RF2 Distance: ");
-    Serial.print(curReading2);
-    Serial.println(" m"); */
-    float change1 = abs(curReading1 - prevReading1);
-    float change2 = abs(curReading2 - prevReading2);
+    float change1 = prevReading1-curReading1;
+    float change2 = prevReading2-curReading2;
     dt = 0;
-    if(change1 >= minChange && !detected) {
+    if(change1 >= minChange && detected != true) {
       t1=millis();
       d1 = curReading1;
+      detected=true;
+      Serial.println(d1);
     }
-    if(change2 > minChange && detected) {
+    if(change2 >= minChange && detected == true) {
       t2=millis();
       dt = t2-t1;
       dt = dt * 0.001;
       d2 = curReading2;
+      Serial.println(d2);
+      Serial.println(detected);
       dx = sqrt(pow(x,2)+pow(abs(d1-d2),2));
       Serial.println(dx);
       v = dx/dt;
