b600a4ea0c6440bb0136836eae239aeccc42352d
Nishil Shah <nishil.shah17@gmail.com>
2015-04-08 16:34:34 -0400

    new stop watch library

--- Parallax/Parallax.ino
+++ Parallax/Parallax.ino
@@ -16,15 +16,16 @@ SoftwareSerial rf1Serial(SERIAL_RX_PIN, SERIAL_TX_PIN);
 SF02 RF2;
 SoftwareSerial rf2Serial(SERIAL_RX_PIN_2, SERIAL_TX_PIN_2);
 
-elapsedMillis timer();
 #define interval 20
-unsigned long t1, t2, dt;
+unsigned long t1;
+unsigned long dt;
+double timeElapsed;
 float curReading1, curReading2;
 float prevReading1 = 0.0, prevReading2 = 0.0;
 float d1, d2, dx;
 float v = 0.0;
 float x = 0.127; //distance in meters between rangefinders
-float minChange = 1.0;
+float minChange = 0.7;
 float minVelocity = 2.0;
 boolean detected = false;
 
@@ -43,6 +44,7 @@ void setup()
 
 void loop()
 {
+    dx = 0;
     delay(100);
     curReading1 = RF1.getAnalogDistance()-2.8;
     curReading2 = RF2.getAnalogDistance()-2.8;
@@ -56,26 +58,33 @@ void loop()
     if(change1 >= minChange && detected != true) {
       t1=micros();
       d1 = curReading1;
-      Serial.println(t1);
       detected=true;
-      //Serial.println(d1);
     }
     if(change2 >= minChange && detected == true) {
-      t2=micros();
-      dt = t2-t1;
-      Serial.println(t2);
+      dt = micros()-t1;
+      timeElapsed = (double) (dt/1000);
+      Serial.print("dt: ");
       Serial.println(dt);
-      //Serial.println(dt);
       d2 = curReading2;
-      //Serial.println(d2);
-      dx = sqrt(pow(x,2)+pow(abs(d1-d2),2));
-      //Serial.println(dx);
-      v = dx/dt;
+      Serial.print("d1: ");
+      Serial.print(d1);
+      Serial.print(", d2: ");
+      Serial.println(d2);
+      double diff = d1-d2;
+      if(diff < 0) {
+        diff = -1*diff;
+      }
+      dx = sqrt(pow(x,2)+pow(diff,2));
+      Serial.print("dx: ");
+      Serial.println(dx,10);
+      v = dx/(timeElapsed);
+      //Serial.print("v: ");
+      //Serial.println(v);
       detected = false;
-      Serial.println(v);
+      Serial.println(detected);
     }
     if(dt > 0 && v > minVelocity) {
-      Serial.print("beep");
+      //Serial.println("beep"); //make sound with buzzer
     }
     prevReading1 = curReading1;
     prevReading2 = curReading2;
